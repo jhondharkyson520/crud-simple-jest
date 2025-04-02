1fd6628b6aa0acb063060b9b4c15eb264174181c
Jhon <jhondharkyson@gmail.com>
2025-04-02 15:57:25 -0300

    Feat: add delete-controller

---
+++ src/controllers/user/delete/delete-controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { DeleteUser } from "../../../use-cases/user/delete/delete";
+
+export const deleteUserController = (deleteUser: DeleteUser) => {
+    return async (req: Request, res: Response): Promise<Response> => {
+        try{
+            const {id} = req.params;
+            const userDelete = await deleteUser.execute(id);
+            return res.status(201).json(userDelete);
+        } catch(error) {
+            return res.status(500).json({error: 'Internal Server Error'})
+        }
+    }
+}
